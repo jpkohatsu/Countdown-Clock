6fa3366a48f958392f2ea2fa5a9612f7e51a42e4
Joseph Kohatsu <jpkohatsu@gmail.com>
2018-02-01 13:11:22 -0600

    let and const variables added

---
+++ scripts.js
@@ -0,0 +1,5 @@
+let countdown;
+const timerDisplay = document.querySelector('.display_time-left');
+const endTime = document.querySelector('.display_end-time');
+const buttons = document.querySelector('[data-time]');
+
